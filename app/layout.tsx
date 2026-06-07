2536237ad7976b6c8395216846793a20caaf9a9c
Ahmet Rifat <almancadunyamm@gmail.com>
2026-06-08 02:23:11 +0300

    feat: PWA manifest ve meta tagları eklendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,16 @@ export const metadata: Metadata = {
   title: "Almanca Okulum",
   description: "Almanca öğrenme ve TELC hazırlık platformu",
   icons: {
-  icon: "/icon.png",
-},
+    icon: "/images/icon.png",
+    apple: "/images/icon-192.png",
+  },
+  manifest: "/manifest.json",
+  themeColor: "#059669",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Almanca Okulum",
+  },
 };
 
 export default function RootLayout({
